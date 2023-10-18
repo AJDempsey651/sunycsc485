08d0086e16e829478546b0f8765312b108e5ed4c
Adam Dempsey <dempseya651@gmail.com>
2023-10-18 15:28:44 -0400

    hw13 submission for part 1
    fixed all applicable lint problems

--- sunycsc485/tests/hw12/hw12_test.py
+++ sunycsc485/tests/hw12/hw12_test.py
@@ -1,5 +1,9 @@
 from sunycsc485.projects.hw12.hw12 import compute_complexity
 import pytest
-@pytest.mark.parametrize('compute_complexity', ['~', '@', '#', '$', '%', '^', '&', '-', '_', '+', '='])
+
+
+@pytest.mark.parametrize('compute_complexity', ['~', '@', '#', '$', '%',
+                                                '^', '&', '-', '_', '+', '='])
 def test_complexifiers(compute_complexity):
-    assert compute_complexity == '~' or '@' or '#' or '$' or '%' or '^' or '&' or '-' or '_' or '+' or '='
+    assert (compute_complexity == '~' or '@' or '#' or '$' or '%' or '^'
+            or '&' or '-' or '_' or '+' or '=')
