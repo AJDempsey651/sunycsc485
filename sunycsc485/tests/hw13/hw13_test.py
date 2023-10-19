ded13b4982d9d28788623cad30947920f02f8267
Adam Dempsey <dempseya651@gmail.com>
2023-10-19 17:17:05 -0400

    hw13 submission for part 2
    Parametrized test cases for hw13 code

---
+++ sunycsc485/tests/hw13/hw13_test.py
@@ -0,0 +1,27 @@
+from sunycsc485.projects.hw13.hw13 import compute_complexity, evaluate_strength
+import pytest
+
+
+@pytest.mark.parametrize('complexity', [0])
+def evaluate_strength_test(complexity):
+    """Test that evaluate_strength will return false if passwords do not meet the strength threshold"""
+    assert evaluate_strength(complexity) == False
+
+
+@pytest.mark.parametrize('complexity', [50])
+def evaluate_strength_test(complexity):
+    """Test that evaluate_strength will return true is passwords meet the strength threshold"""
+    assert evaluate_strength(complexity) == True
+
+
+@pytest.mark.parametrize('passwords', ['password1', 'password2'])
+def test_complexity_0_strength(passwords):
+    """Test that compute_complexity will return 0 if there are no complexifiers"""
+    assert compute_complexity(passwords) == 0.0
+
+
+@pytest.mark.parametrize('passwords', ['123@#%', 'abcde@#$%^'])
+def test_complexity_50_strength(passwords):
+    """Test that compute_complexity will return 50.0 is there is an even number of
+    complexifiers to other characters"""
+    assert compute_complexity(passwords) == 50.0
