27fde95630271628125667a42ea32a2264c267b4
Adam Dempsey <dempseya651@gmail.com>
2023-11-01 19:31:35 -0400

    hw13 submission
    hw13 updated

--- sunycsc485/tests/hw13/hw13_test.py
+++ sunycsc485/tests/hw13/hw13_test.py
@@ -3,25 +3,28 @@ import pytest
 
 
 @pytest.mark.parametrize('complexity', [0])
-def evaluate_strength_test(complexity):
-    """Test that evaluate_strength will return false if passwords do not meet the strength threshold"""
+def test_evaluate_strength_low(complexity):
+    """Test that evaluate_strength will return false if passwords do not meet
+    the strength threshold"""
     assert evaluate_strength(complexity) == False
 
 
 @pytest.mark.parametrize('complexity', [50])
-def evaluate_strength_test(complexity):
-    """Test that evaluate_strength will return true is passwords meet the strength threshold"""
+def test_evaluate_strength_high(complexity):
+    """Test that evaluate_strength will return true is passwords meet the
+     strength threshold"""
     assert evaluate_strength(complexity) == True
 
 
 @pytest.mark.parametrize('passwords', ['password1', 'password2'])
-def test_complexity_0_strength(passwords):
-    """Test that compute_complexity will return 0 if there are no complexifiers"""
+def test_complexity_strength_low(passwords):
+    """Test that compute_complexity will return 0 if there are no
+    complexifiers"""
     assert compute_complexity(passwords) == 0.0
 
 
 @pytest.mark.parametrize('passwords', ['123@#%', 'abcde@#$%^'])
-def test_complexity_50_strength(passwords):
-    """Test that compute_complexity will return 50.0 is there is an even number of
-    complexifiers to other characters"""
+def test_complexity_strength_high(passwords):
+    """Test that compute_complexity will return 50.0 is there is an even
+    number of complexifiers to other characters"""
     assert compute_complexity(passwords) == 50.0
